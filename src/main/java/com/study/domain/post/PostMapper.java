4cc58fe3cb145be14e5433314a73719be4a0d952
love NUNO <nuno08@gmail.com>
2022-12-16 15:11:34 +0900

    Initial commit

---
+++ src/main/java/com/study/domain/post/PostMapper.java
@@ -0,0 +1,21 @@
+package com.study.domain.post;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface PostMapper {
+
+    void save(PostRequest params);
+
+    PostResponse findById(Long id);
+
+    void update(PostRequest params);
+
+    void deleteById(Long id);
+
+    List<PostResponse> findAll();
+
+    int count();
+}
